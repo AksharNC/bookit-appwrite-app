754dc14793c6080909eacd4270b1beff98590f8c
Akshar Chauhan <onak.off494@gmail.com>
2025-04-17 21:46:26 +0530

    Initial commit for bookit-appwrite project

---
+++ context/authContext.js
@@ -0,0 +1,40 @@
+import checkAuth from "@/app/actions/checkAuth";
+import {createContext, useContext, useEffect, useState} from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            const {isAuthenticated, user} = await checkAuth();
+            setIsAuthenticated(isAuthenticated);
+            setCurrentUser(user);
+        };
+
+        checkAuthentication();
+    }, []);
+
+    return (
+        <AuthContext.Provider
+            value={{
+                isAuthenticated,
+                setIsAuthenticated,
+                currentUser,
+                setCurrentUser,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+};
